6d5c3c0b5743b709c1a83222377928f0ba6f7592
yiranshu <shuyiran1425@hotmail.com>
2025-06-29 13:39:42 -0400

    Add muted opening video and update BackgroundVideo component

--- client/src/components/BackgroundVideo.jsx
+++ client/src/components/BackgroundVideo.jsx
@@ -7,7 +7,9 @@ export default function BackgroundVideo(){
     return(
         <div>
             <video autoPlay loop muted playsInline className="back_video" onCanPlayThrough={(event) => event.target.play()}>
-            <source src='https://d3sq57km3exo6v.cloudfront.net/opening_muted.mp4' type="video/mp4"/>Your browser does not support the video tag.
+            <source src='/images/opening_muted_output.mp4' type="video/mp4"/>Your browser does not support the video tag.
+          
+              {/* <source src='https://d3sq57km3exo6v.cloudfront.net/opening_muted.mp4' type="video/mp4"/>Your browser does not support the video tag. */}
             </video>
         
             {!isHomePage && <div className="video_filter"></div>}
